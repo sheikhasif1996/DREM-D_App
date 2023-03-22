0464dbf5e359134f27a77a00503ad0f2e471e41f
sheikh asif <sheikhasif.buet13@gmail.com>
2023-03-22 08:52:12 -0500

    updated

--- webApp/auth.py
+++ webApp/auth.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login',methods=['GET', 'POST'])
 def login():
     user = {'name': 'Sheikh', 'age': 30}
     return render_template("login.html", user=user)
@@ -12,7 +12,7 @@ def logout():
     user = {'name': 'Sheikh', 'age': 30}
     return "<p> Logout </p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up',methods=['GET', 'POST'])
 def sign_up():
     user = {'name': 'Sheikh', 'age': 30}
     return render_template("sign-up.html", user=user)
